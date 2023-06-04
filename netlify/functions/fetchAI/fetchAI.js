c4195de545d9dcce5efb0c48c2d929d4c67b0b17
Santiago Domínguez Barrios <Santiago.Dominguez.Barrios@gmail.com>
2023-06-04 14:50:55 +0200

    Cambiado fetchAI.js para invocar a API de OpenAI

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,16 +1,33 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const response = await openai.createCompletion({
+        model: 'davinci:ft-personal-2023-06-04-08-44-37',
+        prompt: event.body,
+        presence_penalty: 0,
+        frequency_penalty: 0.3,
+        max_tokens: 100,
+        temperature: 0,
+        stop: [' END', '->']
+    })
+
     return {
       statusCode: 200,
       headers: {
         "Access-Control-Allow-Origin": "*"
       },
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify(
+        {
+          reply: response.data
+        }
+      ),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
