bb9aacce863ee747086b4fa40b7470a06b324d81
Santiago Domínguez Barrios <Santiago.Dominguez.Barrios@gmail.com>
2023-06-04 14:37:07 +0200

    Añadido control de accesos a fetchAI.js

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -4,6 +4,9 @@ const handler = async (event) => {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
       body: JSON.stringify({ message: `Hello ${subject}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
